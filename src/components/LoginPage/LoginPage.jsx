0e9d945511cb438745ec1f6c5280b7efd6a877d0
dyamorph <dyamorph22@gmail.com>
2023-02-16 19:17:10 +0300

    refactor login page design

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -26,60 +26,66 @@ const LoginPage = () => {
       alert("Failure to login");
     }
 
-    window.localStorage.setItem("token", data.payload.token);
+    if ("token" in data.payload) {
+      window.localStorage.setItem("token", data.payload.token);
+    }
   };
 
   if (isAuth) {
     return <Navigate to={"/collection"} />;
   }
-  console.log('isAuth: ', isAuth);
+  console.log(isAuth);
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)} className={styles.login_form}>
-        <h1 className={styles.login_heading}>Login Form</h1>
-        <div className="mb-6">
-          <label className={styles.email_label} htmlFor="email">
-            Your email
-          </label>
-          <input
-            className={styles.email_input}
-            {...register("email", { required: "Enter your email" })}
-            type="email"
-            id="email"
-            placeholder="name@example.com"
-            required
-          ></input>
-        </div>
-        <div className="mb-6">
-          <label className={styles.password_label} htmlFor="password">
-            Your password
-          </label>
-          <input
-            className={styles.password_input}
-            {...register("password", { required: "Enter your password" })}
-            type="password"
-            id="password"
-            required
-          ></input>
-        </div>
-        <div className="flex items-start mb-6">
-          <div className="flex items-center h-5">
+      <section className={styles.login_page_content}>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.login_form}>
+          <h1 className={styles.login_heading}>Login</h1>
+          <div className="mb-6">
+            <label className={styles.email_label} htmlFor="email">
+              Your email
+            </label>
             <input
-              className={styles.remember_input}
-              id="remember"
-              type="checkbox"
-              value=""
+              className={styles.email_input}
+              {...register("email", { required: "Enter your email" })}
+              type="email"
+              id="email"
+              placeholder="name@example.com"
               required
             ></input>
           </div>
-          <label className={styles.remember_label} htmlFor="remember">
-            Remember me
-          </label>
-        </div>
-        <button className={styles.submit_btn} type="submit">
-          Submit
-        </button>
-      </form>
+          <div className="mb-6">
+            <label className={styles.password_label} htmlFor="password">
+              Your password
+            </label>
+            <input
+              className={styles.password_input}
+              {...register("password", { required: "Enter your password" })}
+              type="password"
+              id="password"
+              required
+            ></input>
+          </div>
+          <div className="flex items-start mb-6">
+            <div className="flex items-center h-5">
+              <input
+                className={styles.remember_input}
+                id="remember"
+                type="checkbox"
+                value=""
+                required
+              ></input>
+            </div>
+            <label className={styles.remember_label} htmlFor="remember">
+              Remember me
+            </label>
+          </div>
+          <div className={styles.btn_container}>
+            <button className={styles.login_btn}>
+              <span className={styles.login_btn_inner}>Login</span>
+            </button>
+          </div>
+        </form>
+      </section>
     </>
   );
 };
